c3d35db401d83321ed5accac889b9ebc768db0e4
Jérémie P <jpourageaux@hotmail.fr>
2025-02-05 22:15:07 +0100

    add nav and top job card

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,29 @@
 <x-layout>
-    Hello World
+    <section>
+        <h3 class="mb-6">Top jobs</h3>
+
+        <div class="flex flex-col items-center rounded-xl p-4 bg-white/5 text-center">
+            <div class="self-start text-sm">Laracast</div>
+
+            <div class="text-xl font-semibold py-8">
+                <h3>Video Producer</h3>
+                <p>full time - From $60000</p>
+            </div>
+
+            <div class="flex justify-between items-center w-full">
+                <div class="flex gap-3">
+                    <a class='bg-white/10 hover:bg-white/25 px-2 py-1 rounded-lg text-xs transition-colors duration-300'
+                       href="">Tag</a>
+                    <a class='bg-white/10 hover:bg-white/25 px-2 py-1 rounded-lg text-xs transition-colors duration-300'
+                       href="">Tag</a>
+                    <a class='bg-white/10 hover:bg-white/25 px-2 py-1 rounded-lg text-xs transition-colors duration-300'
+                       href="">Tag</a>
+                    <a class='bg-white/10 hover:bg-white/25 px-2 py-1 rounded-lg text-xs transition-colors duration-300'
+                       href="">Tag</a>
+                </div>
+
+                <img class='rounded rounded-xl' src="https://placehold.co/42x42" alt=""/>
+            </div>
+        </div>
+    </section>
 </x-layout>
