874b1fce79f8a2081783ca6265976eb644176bb8
Jérémie P <jpourageaux@hotmail.fr>
2025-02-06 11:45:44 +0100

    add hover border smooth transition on cards

--- resources/views/components/job-card-wild.blade.php
+++ resources/views/components/job-card-wild.blade.php
@@ -1,11 +1,11 @@
-<div class="flex gap-x-6 rounded-xl p-4 bg-white/5">
+<div class="flex gap-x-6 rounded-xl p-4 bg-white/5 border border-transparent hover:border-blue-600 group transition-colors duration-300">
     <div>
         <x-employer-logo :width='100'/>
     </div>
 
     <div class="flex flex-1 flex-col">
         <a href='/' class="text-gray-400 text-sm">Laracast</a>
-        <h3 class="font-bold text-xl mt-3">Video Producer</h3>
+        <h3 class="font-bold text-xl mt-3 group-hover:text-blue-600 transition-colors duration-300">Video Producer</h3>
         <p class="mt-auto">full time - From $60000</p>
     </div>
 
--- resources/views/components/job-card.blade.php
+++ resources/views/components/job-card.blade.php
@@ -1,9 +1,9 @@
-<div class="flex flex-col items-center rounded-xl p-4 bg-white/5 text-center">
+<div class="flex flex-col items-center rounded-xl p-4 bg-white/5 text-center border border-transparent hover:border-blue-600 group transition-colors duration-300">
     <div class="self-start text-sm">Laracast</div>
 
-    <div class="font-bold py-8">
-        <h3>Video Producer</h3>
-        <p>full time - From $60000</p>
+    <div class="py-8">
+        <h3 class="group-hover:text-blue-600 text-xl font-bold transition-colors duration-300">Video Producer</h3>
+        <p class="text-sm mt-4">full time - From $60000</p>
     </div>
 
     <div class="flex justify-between items-center w-full">
