23b6b03f3a1a9d48ad271eab84d769d865278308
Jérémie P <jpourageaux@hotmail.fr>
2025-02-06 12:25:03 +0100

    add search input

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,14 @@
 <x-layout>
     <div class="space-y-10">
-        <section>
+        <section class="text-center pt-6">
+            <H1 class="text-4xl">Lets Find Your Next Job </H1>
+            <form action="" class="mt-6">
+                <input type="text" placeholder="Web Developer..."
+                       class="rounded-xl bg-white/5 border-white/10 px-5 py-4 w-full max-w-xl">
+            </form>
+        </section>
+
+        <section class="pt-6">
             <x-section-heading>Featured Jobs</x-section-heading>
 
             <div class="grid sm:grid-cols-2 lg:grid-cols-3 gap-8 mt-6">
