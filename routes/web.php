261eab5bfc97e99a641c0f1c24f70999af1aac4c
Jérémie P <jpourageaux@hotmail.fr>
2025-02-07 14:55:47 +0100

    add new endpoint to register and login

--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,10 @@ use App\Http\Controllers\JobController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [JobController::class, 'index']);
+
+Route::get('/register', [RegisteredUserController::class, 'create']);
+Route::post('/register', [RegisteredUserController::class, 'store']);
+
+Route::get('/login', [SessionController::class, 'create']);
+Route::post('/login', [SessionController::class, 'store']);
+Route::delete('/login', [SessionController::class, 'destroy']);
